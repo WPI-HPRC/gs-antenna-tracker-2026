8867f408f7b5a3ad43e383edd60bfa2c69212e46
jacubb5 <jacubb5@outlook.com>
2026-04-02 19:50:13 -0400

    Updated chassis code, removed StepperDriver class

--- lib/Chassis/src/chassis.cpp
+++ lib/Chassis/src/chassis.cpp
@@ -1,11 +1,13 @@
 
 #include "chassis.h"
-#include "stepperDriver.h"
 #include <AccelStepper.h>
 
 IntervalTimer chassisTimer;
 stepperDriver azimuthStepper;
 stepperDriver elevationStepper;
+
+AccelStepper azStepper(AccelStepper::DRIVER, this->AZ_PULSE_PIN, this->AZ_DIR_PIN);
+AccelStepper elStepper(AccelStepper::DRIVER, this->EL_PULSE_PIN, this->EL_DIR_PIN);
 
 
 Chassis::Chassis(void) {
@@ -14,27 +16,52 @@ Chassis::Chassis(void) {
 
 void Chassis::initializeChassis(void) {
     chassisTimer.begin(chassisLoop, 10000); // Call chassisLoop every 10ms
+
+    azStepper.setEnablePin(this->AZ_ENABLE_PIN);
+    elStepper.setEnablePin(this->EL_ENABLE_PIN);
+
+    azStepper.setMaxSpeed(this->azimuthMaxSpeed);
+    elStepper.setMaxSpeed(this->elevationMaxSpeed);
+
+    azStepper.setAcceleration(this->azimuthAcceleration);
+    elStepper.setAcceleration(this->elevationAcceleration);
+
+    azStepper.setMinPulseWidth(5); // microseconds
+    elStepper.setMinPulseWidth(5); // microseconds
+
+    // TODO: Update this
+    azStepper.setPinsInverted(false, false, true);
+    elStepper.setPinsInverted(false, false, true);
 }
 
 void Chassis::chassisLoop(void) {
     Chassis::updateChassis();
+
+    // Add additional code per update cycle, such as reading encoder values and handling alarms
+    // Encoder::readEncoders();
 }
 
 void Chassis::updateChassis(void) {
-    // Code to update the chassis state
+    azStepper.runSpeed();
+    elStepper.runSpeed();
 }
 
 void Chassis::setChassisSpeed(float azSpeed, float elSpeed) {
-    // Code to set the speed of the chassis based on input speeds
+    azStepper.setSpeed(azSpeed);
+    elStepper.setSpeed(elSpeed);
 }
 
 void Chassis::stopChassis(void) {
-    Chassis::setChassisSpeed(0.0, 0.0);
+    azStepper.stop();
+    elStepper.stop();
 }
 
-void Chassis::chassisESTOP(void) {
-    // Code to immediately stop the chassis in case of an emergency
-    Chassis::stopChassis();
+void Chassis::freeChassis(void) {
+    azStepper.disableOutputs();
+    elStepper.disableOutputs();
+}
 
-    // TODO: Add additional code to handle alarm signal and disabling motor power
+void Chassis::calibrateChassis(void) {
+    // TODO: Update this
+    elStepper.setCurrentPosition(0);
 }
--- lib/Chassis/src/chassis.h
+++ lib/Chassis/src/chassis.h
@@ -15,29 +15,29 @@ protected:
     
     void chassisLoop(void);
     void updateChassis(void);
-
-    float azimuthSpeed; // rad/s
-    float elevationSpeed; // rad/s
     
     float azimuthMaxSpeed = 0.1; // rad/s
-    float elevationMaxSpeed = 0.1; // rad/s
+    float elMaxSpeed = 0.1; // rad/s
+
+    float azAcceleration = 0.1; // rad/s^2
+    float elAcceleration = 0.1; // rad/s^2
 
     /// Define pin numbers
-    int azEncAPin = 33;
-    int azEncBPin = 32;
-    int azAlarmPin = 28;
-    int elEncAPin = 30;
-    int elEncBPin = 31;
-    int elAlarmPin = 22;
+    int AZ_ENC_A_PIN = 33;
+    int AZ_ENC_B_PIN = 32;
+    int AZ_ALARM_PIN = 28;
+    int EL_ENC_A_PIN = 30;
+    int EL_ENC_B_PIN = 31;
+    int EL_ALARM_PIN = 22;
 
-    int elPulsePin = 6;
-    int elDirPin = 9;
-    int elEnablePin = 12;
-    int azPulsePin = 11;
-    int azDirPin = 13;
-    int azEnablePin = 10;
+    int EL_PULSE_PIN = 6;
+    int EL_DIR_PIN = 9;
+    int EL_ENABLE_PIN = 12;
+    int AZ_PULSE_PIN = 11;
+    int AZ_DIR_PIN = 13;
+    int AZ_ENABLE_PIN = 10;
 
-    int CWHallPin = 18;
-    int CCWHallPin = 19;
+    int CW_HALL_PIN = 18;
+    int CCW_HALL_PIN = 19;
 
 };
