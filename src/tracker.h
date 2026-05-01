2a7cd8b1681a096925ed6735a915f89894cf4a81
jacubbage <jacubbage@wpi.edu>
2026-04-30 20:36:41 -0400

    (Somwhat) driving motors

--- src/tracker.h
+++ src/tracker.h
@@ -1,29 +1,29 @@
-/**
- * Holds all the function prototypes and variables related to tracker control and the state machine.
- * There should be no function bodies defined here, only variables and prototypes.
- */
+// /**
+//  * Holds all the function prototypes and variables related to tracker control and the state machine.
+//  * There should be no function bodies defined here, only variables and prototypes.
+//  */
 
-#pragma once // This replaces #ifndef...
+// #pragma once // This replaces #ifndef...
 
-class Tracker
-{
-public:
-    Tracker(void);
-    void InitializeTracker(void);
-    void TrackerLoop(void);
+// class Tracker
+// {
+// public:
+//     Tracker(void);
+//     void InitializeTracker(void);
+//     void TrackerLoop(void);
 
-protected:
-    void EnterIdleState(void);
-    void EnterCalibratingState(void);
-    void EnterTrackingState(void);
+// protected:
+//     void EnterIdleState(void);
+//     void EnterCalibratingState(void);
+//     void EnterTrackingState(void);
 
-    // Enumerate tracker states
-    enum TRACKER_STATE
-    {
-        TRACKER_IDLE,
-        TRACKER_CALIBRATING,
-        TRACKER_TRACKING
-    };
+//     // Enumerate tracker states
+//     enum TRACKER_STATE
+//     {
+//         TRACKER_IDLE,
+//         TRACKER_CALIBRATING,
+//         TRACKER_TRACKING
+//     };
 
-    TRACKER_STATE trackerState = TRACKER_IDLE;
-}
+//     TRACKER_STATE trackerState = TRACKER_IDLE;
+// }
